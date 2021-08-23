836f8e70d9dca798d9b7876e6a9e81222a66275b
Administrator <Administrator@192.168.0.2>
2021-08-23 20:22:03 -0300

    teste2

--- Projetojava2/src/curso/jdev/prj2/Pessoa.java
+++ Projetojava2/src/curso/jdev/prj2/Pessoa.java
@@ -1,5 +1,28 @@
 package curso.jdev.prj2;
 
 public class Pessoa {
+	
+	private String nome;
+	
+	private String sobreNome;
+
+	public String getNome() {
+		return nome;
+	}
+	
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobreNome() {
+		return sobreNome;
+	}
+
+	public void setSobreNome(String sobreNome) {
+		this.sobreNome = sobreNome;
+	}
+	
+	
 
 }
