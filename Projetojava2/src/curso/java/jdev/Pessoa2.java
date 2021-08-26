676528209a587f85cd2bd51036786bafe12bdb95
Administrator <Administrator@10.161.111.205>
2021-08-26 18:51:24 -0300

    criação de novo arquivo

--- Projetojava2/src/curso/java/jdev/Pessoa2.java
+++ Projetojava2/src/curso/java/jdev/Pessoa2.java
@@ -1,26 +1,26 @@
-package curso.jdev.prj2;
+package curso.java.jdev;
 
-public class Pessoa {
-	
+public class Pessoa2 {
+
 	private String nome;
 	private String sobreNome;
-	
+	private int idade;
 	public String getNome() {
 		return nome;
 	}
-	
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
 	public String getSobreNome() {
 		return sobreNome;
 	}
-
 	public void setSobreNome(String sobreNome) {
 		this.sobreNome = sobreNome;
 	}
-	
-	
-
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
 }
