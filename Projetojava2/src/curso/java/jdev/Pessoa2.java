d7cc673ecdba024ce51333410e6ad039206800ac
Administrator <Administrator@10.161.111.205>
2021-08-26 19:08:19 -0300

    testes de git

--- Projetojava2/src/curso/java/jdev/Pessoa2.java
+++ Projetojava2/src/curso/java/jdev/Pessoa2.java
@@ -1,10 +1,15 @@
 package curso.java.jdev;
 
+import java.util.Date;
+
 public class Pessoa2 {
 
 	private String nome;
 	private String sobreNome;
 	private int idade;
+	private Date dataNacimento;
+	private String profissao;
+	
 	public String getNome() {
 		return nome;
 	}
@@ -22,5 +27,17 @@ public class Pessoa2 {
 	}
 	public void setIdade(int idade) {
 		this.idade = idade;
+	}
+	public Date getDataNacimento() {
+		return dataNacimento;
+	}
+	public void setDataNacimento(Date dataNacimento) {
+		this.dataNacimento = dataNacimento;
+	}
+	public String getProfissao() {
+		return profissao;
+	}
+	public void setProfissao(String profissao) {
+		this.profissao = profissao;
 	}
 }
